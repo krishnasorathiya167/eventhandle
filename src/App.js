2402adffd2b10d02f6bb33b4b517ba8e130ff518
krishna <krishnasorathiya4415@gmail.com>
2024-06-28 10:52:17 +0530

    eventhandle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react'
 
-function App() {
+
+const App = () => {
+  // useref
+  let name = useRef();
+  let age = useRef();
+
+  let submit = () => {
+    let data = {
+      name: name.current.value,
+      age: age.current.value,
+    }
+    console.log(data);
+  }
+
+  // usestate
+
+  const [text, settext] = useState({})
+
+  let handle = (e) => {
+    settext({ ...text, [e.target.name]: e.target.value })
+  }
+
+  let submitt = () => {
+    console.log(text);
+  }
+
+  // useeffect
+
+  const [count, setcount] = useState(0)
+
+  useEffect
+  (() => {
+    setInterval(() => {
+        if (count <= 10) {
+            setcount(count + 1)
+        }
+    }, 1000)
+},[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>useref</h1>
+      <label>Name : <input type="text" ref={name} /></label><br /><br />
+      <label>Age : <input type="text" ref={age} /></label><br /><br />
+      <button onClick={submit}>Submit</button><br /><br />
+
+      <h1>useState</h1>
+      <p>{text.name}</p>
+      <p>{text.age}</p>
+      <label>Name : <input type="text" name='name' onChange={handle} /></label><br /><br />
+      <label>Age : <input type="text" name='age' onChange={handle} /></label><br /><br />
+      <button onClick={submitt}>Submit</button><br /><br />
+
+      <h1>useEffect</h1>
+      <p>{count}</p>
+    </>
+  )
 }
 
 export default App;
